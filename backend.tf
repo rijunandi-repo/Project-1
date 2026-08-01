c75cc9f5b0655e1c154461dfe17dcea2856920d0
RIJU NANDI <it.20203012@gmail.com>
2026-08-01 21:32:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "young-minds-app-terraform-state"
+    bucket         = "riju-nandi-yg-1"
     region         = "us-east-1"
     key            = "batch-32/terraform.tfstate"
     encrypt        = true
